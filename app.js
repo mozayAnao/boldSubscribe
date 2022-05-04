451ea167df9e6bf1753de28829b7da5c48dfc325
mozay <mosesanao13@gmail.com>
2022-05-04 20:50:19 +0000

    wrap up

--- app.js
+++ app.js
@@ -6,23 +6,36 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 const config = require('config');
 
+var app = express();
+
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
   process.exit(1);
 }
 
+const environment = app.get('env');
+console.log(environment);
+
+let dbConnectionString = '';
+
+if (environment === 'development') {
+  dbConnectionString = config.get('dbConnectionString');
+  console.log(dbConnectionString);
+} else {
+  dbConnectionString = config.get('dbConnectionString-Prod');
+  console.log(dbConnectionString);
+}
+
 mongoose
-  .connect(config.get('dbConnectionString'))
+  .connect(dbConnectionString)
   .then(() => console.log('Connected to MongoDB...'))
-  .catch((err) => console.error('Could not connect to MongoDB'));
+  .catch((err) => console.error('Could not connect to MongoDB', err));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const schoolsRouter = require('./routes/schools');
 const paymentsRouter = require('./routes/payments');
 const authRouter = require('./routes/auth');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
