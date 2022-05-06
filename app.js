67b4b1289517bd0b3271f002349224beddec837e
mozay <mosesanao13@gmail.com>
2022-05-06 16:41:17 +0000

    script

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const config = require('config');
 
 var app = express();
 
-app.use(cors());
-
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
   process.exit(1);
@@ -44,6 +42,7 @@ const authRouter = require('./routes/auth');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
